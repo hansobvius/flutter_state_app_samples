6ba84f91a50784406e841e23f2eab668983d14cd
Thiago Caiado <thiago.caiado@hotmail.com>
2026-01-19 19:47:11 -0300

    correction view model paht name

--- listenable_builder/lib/advanced/application/ui/home.dart
+++ listenable_builder/lib/advanced/application/ui/home.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Advanced BloC Example"),
+        title: Text("Advanced Listenable Builder Example"),
       ),
       body: Center(
         child: Column(
